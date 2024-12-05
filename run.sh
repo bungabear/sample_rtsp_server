3e81eba8874e896c4bec3583c2de47a7195723d6
haesungJoo <haesung0728@gmail.com>
2024-12-05 16:44:24 +0900

    RTSP user option added

--- run.sh
+++ run.sh
@@ -1,11 +1,14 @@
 #/bin/sh
 
+echo "Start initalize option for ffmpeg"
+
 sleepS=2
 video="smptebars=size=1920x1080"
 smptebarOption="-f lavfi"
+rtspURL="rtsp://localhost:8554/mystream"
 
 # 옵션 처리하기
-while getopts "v:s:" opt; do
+while getopts "v:s:u" opt; do
   case $opt in
     v)
       video=$OPTARG
@@ -13,12 +16,17 @@ while getopts "v:s:" opt; do
     s)
       sleepS=$OPTARG
       ;;
+    u)
+      rtspURL="rtsp://user:user@localhost:8554/mystream"
+      ;;
     \?)
       echo "Usage: $0 [-v videofile default is smptebars=size=1920x1080] [-s sleepS default is 2]"
       exit 1
       ;;
   esac
 done
+
+echo "Finish initalize option for ffmpeg"
 
 echo The video option is $video
 
@@ -34,4 +42,4 @@ esac
 /mediamtx & \
 echo sleep $sleepS for server ready ; \
 sleep $sleepS ; \
-ffmpeg -re -stream_loop -1 $smptebarOption -i $video -vcodec libx264 -vf "drawtext=fontsize=60:fontcolor=yellow:text='%{localtime\:%T}':x=(w-text_w):y=(h-text_h),drawtext=fontsize=60:fontcolor=yellow:text='%{eif\:w\:d}x%{eif\:h\:d}':y=(h-text_h)" -vcodec libx264 -f rtsp rtsp://localhost:8554/mystream ;
+ffmpeg -re -stream_loop -1 $smptebarOption -i $video -vcodec libx264 -vf "drawtext=fontsize=60:fontcolor=yellow:text='%{localtime\:%T}':x=(w-text_w):y=(h-text_h),drawtext=fontsize=60:fontcolor=yellow:text='%{eif\:w\:d}x%{eif\:h\:d}':y=(h-text_h)" -vcodec libx264 -f rtsp $rtspURL ;
