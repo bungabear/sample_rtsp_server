fa8838ef846f21210eb08f0ba12f35d8f7b857ab
haesungJoo <haesung0728@gmail.com>
2024-12-06 14:11:09 +0900

    run.sh apped `u` option to control authenticated user

--- run.sh
+++ run.sh
@@ -1,15 +1,15 @@
 #/bin/sh
-
 echo "Start initalize option for ffmpeg"
 
 sleepS=2
 video="smptebars=size=1920x1080"
 smptebarOption="-f lavfi"
 rtspURL="rtsp://localhost:8554/mystream"
+isUOption=false
 
 # 옵션 처리하기
 while getopts "v:s:u" opt; do
-  case $opt in
+  case $opt in 
     v)
       video=$OPTARG
       ;;
@@ -17,14 +17,19 @@ while getopts "v:s:u" opt; do
       sleepS=$OPTARG
       ;;
     u)
-      rtspURL="rtsp://user:user@localhost:8554/mystream"
+      isUOption=true
       ;;
     \?)
-      echo "Usage: $0 [-v videofile default is smptebars=size=1920x1080] [-s sleepS default is 2]"
+      echo "Usage: $0 [-v videofile default is smptebars=size=1920x1080] [-s sleepS default is 2][-u use myuser option]"
       exit 1
       ;;
   esac
 done
+
+if($isUOption); then
+  cp /resource/auth_myuser/mediamtx.yml /mediamtx.yml # auth_myuser 폴더의 mediamtx.yml 파일을 /mediamtx.yml로 복사
+  rtspURL="rtsp://myuser:myuser@localhost:8554/mystream" # rtspURL을 myuser 계정으로 변경
+fi
 
 echo "Finish initalize option for ffmpeg"
 
@@ -39,7 +44,6 @@ case "$video" in
     ;;
 esac
 
-/mediamtx & \
-echo sleep $sleepS for server ready ; \
+/mediamtx & echo sleep $sleepS for server ready ; \
 sleep $sleepS ; \
 ffmpeg -re -stream_loop -1 $smptebarOption -i $video -vcodec libx264 -vf "drawtext=fontsize=60:fontcolor=yellow:text='%{localtime\:%T}':x=(w-text_w):y=(h-text_h),drawtext=fontsize=60:fontcolor=yellow:text='%{eif\:w\:d}x%{eif\:h\:d}':y=(h-text_h)" -vcodec libx264 -f rtsp $rtspURL ;
